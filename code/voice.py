17202480cd174b505665f0f42f68b438134f0fc6
shadofren <xxlaguna93@gmail.com>
2017-11-13 08:36:44 +0800

    utilize command line sound program mpg123 to play sound, pip install in linux, windows need to download the executable and put in the same folder

--- code/voice.py
+++ code/voice.py
@@ -1,16 +1,12 @@
 from gtts import gTTS
-from pygame import mixer
-import time
+import os
 
 def playAudio(text):
-    print("called")
     tts = gTTS(text=text, lang='en')
     file_mp3 = "sound.mp3"
-    time.sleep(0.1)
     tts.save(file_mp3)
-    mixer.init()
-    mixer.music.load(file_mp3)
-    mixer.music.play()
+    os.system("mpg123 " + file_mp3)
+    os.system("clear")
 
 if __name__ == '__main__':
     playAudio("hello")
