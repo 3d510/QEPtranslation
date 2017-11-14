4282d1ee489112e45b284390e5320e478e140d38
Doan Duy Duc <3dmailboxes@gmail.com>
2017-11-14 21:44:45 +0800

    fix

--- code/entry.py
+++ code/entry.py
@@ -2,6 +2,7 @@ import psycopg2
 import easygui
 import json
 import os
+import re
 import glob
 
 from code.translate_qep import translate_qeptree_to_text
@@ -58,38 +59,45 @@ if connectStatus:
                 continue
             print("Query:", query)
             query = query.strip().lower()
-            query_qep = ''
             command = query.split(' ')[0]
-            if command in ['select', 'insert', 'update', 'delete', 'values', 'execute', 'declare']:
+
+            query_qep = None
+            valid_command = re.compile(r"(select|insert|update|delete|values|execute|declare|create table \b.*\b as).*")
+            if bool(re.match(valid_command, query)):
                 query_qep = 'explain (format json) ' + query
-            elif command == 'create' and query.split(' ')[1] == 'table' and query.split(' ')[2] == "as":
-                query_qep = 'explain (format json) ' + query
-            if len(query_qep) == 0:
-                continue
-            cur.execute(query_qep)
-            rows = cur.fetchall()
 
-            for row in rows:
-                qep_index += 1
-                qep_json_path = os.path.join('..', 'data', 'json', file_name + '_' + str(qep_index) + ".json")
-                qep_text_path = os.path.join('..', 'data', 'txt', file_name + '_' + str(qep_index) + ".txt")
-                with open(qep_json_path, 'wt') as out:
-                    res = json.dump(row[0], out, sort_keys=True, indent=2, separators=(',', ': '))
-                translate_qeptree_to_text(qep_json_path, qep_text_path)
+            if query_qep is not None:
+                rows = []
+                try:
+                    cur.execute(query_qep)
+                    rows = cur.fetchall()
+                except Exception as ex:
+                    print("Invalid query, cannot explain")
+                    print("Exception:", ex)
 
-
+                for row in rows:
+                    qep_index += 1
+                    qep_json_path = os.path.join('..', 'data', 'json', file_name + '_' + str(qep_index) + ".json")
+                    qep_text_path = os.path.join('..', 'data', 'txt', file_name + '_' + str(qep_index) + ".txt")
+                    with open(qep_json_path, 'wt') as out:
+                        res = json.dump(row[0], out, sort_keys=True, indent=2, separators=(',', ': '))
+                    translate_qeptree_to_text(qep_json_path, qep_text_path)
 
             print('Executing query...')
-            cur.execute(query)
-            print('Row count: ' + str(cur.rowcount), " - Results:")
-            if cur.rowcount <= 0:
-                print("")
-                continue
-            rows = cur.fetchall()
-            for row in rows:
-                for i in range(len(row)):
-                    print("   ", row[i], end='')
-                print("\n")
+            try:
+                cur.execute(query)
+                print('Row count: ' + str(cur.rowcount), " - Results:")
+                if cur.rowcount <= 0:
+                    print("")
+                    continue
+                rows = cur.fetchall()
+                for row in rows:
+                    for i in range(len(row)):
+                        print("   ", row[i], end='')
+                    print("\n")
+            except Exception as ex:
+                print("Invalid query, cannot execute")
+                print("Exception:", ex)
 
         choice = input("Continue? (Y to select another sql file, N to quit session):\n")
         if choice == 'N':
